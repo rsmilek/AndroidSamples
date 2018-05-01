c589cf88bf9d9a3104e1d4ab0b012b9ad6f41389
rsmilek <rsmilek@users.noreply.github.com>
2018-05-01 15:08:45 +0200

    + Added time, actual speed and other small improvements

--- Location/Location/MainActivity.cs
+++ Location/Location/MainActivity.cs
@@ -7,6 +7,7 @@ using Android.Widget;
 using Android.OS;
 using Android.Locations;
 using Android.Util;
+using Java.Text;
 
 namespace Location
 {
@@ -14,20 +15,22 @@ namespace Location
 	[Activity (Label = "Location", MainLauncher = true)]
 	public class MainActivity : Activity, ILocationListener
 	{
-		LocationManager locMgr;
+		LocationManager locMgr = null;
 		string tag = "MainActivity";
-		Button button;
+		Button btnStartStop;
 		TextView tvLatitude;
 		TextView tvLongitude;
 		TextView tvProvider;
         TextView tvDistance;
+        TextView tvSpeed;
         /// <summary>Stores GPS coordinates of last location update.</summary>
         LatLng lastLatLng = new LatLng();
         /// <summary>Stores GPS coordinates of actrual location update.</summary>
         LatLng actLatLng = new LatLng();
+        /// <summary>Stores begin time measurement.</summary>
+        long startTime = 0;
         /// <summary>Stores whole distance taken from location updates.</summary>
         double? distance = null;
-
 
         protected override void OnCreate(Bundle bundle)
 		{
@@ -36,11 +39,15 @@ namespace Location
 
 			// Set our view from the "Main.axml" layout resource
 			SetContentView(Resource.Layout.Main);
-			button = FindViewById<Button> (Resource.Id.myButton);
-            tvLatitude = FindViewById<TextView> (Resource.Id.latitude);
-            tvLongitude = FindViewById<TextView> (Resource.Id.longitude);
-            tvProvider = FindViewById<TextView> (Resource.Id.provider);
-            tvDistance = FindViewById<TextView>(Resource.Id.distance);
+            btnStartStop = FindViewById<Button> (Resource.Id.btnStartStop);
+            tvLatitude = FindViewById<TextView> (Resource.Id.tvLatitude);
+            tvLongitude = FindViewById<TextView> (Resource.Id.tvLongitude);
+            tvProvider = FindViewById<TextView> (Resource.Id.tvProvider);
+            tvDistance = FindViewById<TextView>(Resource.Id.tvDistance);
+            tvSpeed = FindViewById<TextView>(Resource.Id.tvSpeed);
+
+            // Connect event handlers
+            btnStartStop.Click += delegate { btnStartStop_OnClick(); };
         }
 
         protected override void OnStart()
@@ -49,27 +56,121 @@ namespace Location
 			Log.Debug(tag, "----- OnStart called");
 		}
 
-		// OnResume gets called every time the activity starts, so we'll put our RequestLocationUpdates
-		// code here, so that 
-	    ///<summary></summary>
-		protected override void OnResume()
+        ///<summary>OnResume gets called every time the activity starts, so we'll put our RequestLocationUpdates code here, so that.</summary>
+        protected override void OnResume()
 		{
 			base.OnResume(); 
 			Log.Debug(tag, "----- OnResume called");
 
 			// initialize location manager
-			locMgr = GetSystemService(Context.LocationService) as LocationManager;
+            locMgr = GetSystemService(Context.LocationService) as LocationManager;
             if (locMgr.AllProviders.Contains(LocationManager.GpsProvider))
                 Log.Debug(tag, "----- GpsProvider available");
             if (locMgr.AllProviders.Contains(LocationManager.NetworkProvider))
                 Log.Debug(tag, "----- NetworkProvider available");
             if (locMgr.AllProviders.Contains(LocationManager.PassiveProvider))
                 Log.Debug(tag, "----- PassiveProvider available");
+		}
 
+		protected override void OnPause()
+		{
+			base.OnPause();
 
-            button.Click += delegate {
-				button.Text = "Location Service Running";
+            // stop sending location updates when the application goes into the background
+            // to learn about updating location in the background, refer to the Backgrounding guide
+            // http://docs.xamarin.com/guides/cross-platform/application_fundamentals/backgrounding/
 
+            // RemoveUpdates takes a pending intent - here, we pass the current Activity
+            HandleLocationUpdates(false);
+            Log.Debug(tag, "----- Location updates paused because application is entering the background");
+		}
+
+		protected override void OnStop()
+		{
+			base.OnStop();
+			Log.Debug(tag, "----- OnStop called");
+		}
+
+	    ///<summary>Called when the location has changed.</summary>
+		///<remarks>Implements ILocationListener interface.</remarks>
+		public void OnLocationChanged(Android.Locations.Location location)
+		{
+            if (location == null) return;
+
+            tvLatitude.Text = "Latitude: " + location.Latitude.ToString();
+            tvLongitude.Text = "Longitude: " + location.Longitude.ToString();
+            tvProvider.Text = "Provider: " + location.Provider.ToString();
+
+            //Decide what to do on location update
+            if (!distance.HasValue)
+            {
+                //First location update - do initializations
+                Log.Debug(tag, "----- Location initialized");
+                lastLatLng.Assign(location);
+                startTime = location.Time;
+                distance = 0.0;
+            }
+            else
+            {
+                //Next location updates - calculate distance step as actual - last & whole distance 
+                Log.Debug(tag, "----- Location changed");
+                actLatLng.Assign(location);
+                distance += Utils.HaversineDistance(lastLatLng, actLatLng, Utils.DistanceUnit.Kilometers);
+                lastLatLng.Assign(actLatLng);
+                tvDistance.Text = String.Format("Distance: {0:0.000} Km", distance);
+
+                var testStr = String.Format("{0:0.00} Km/h   ", location.Speed * 3.6);
+                testStr += new Java.Text.SimpleDateFormat("HH:mm:ss").Format(location.Time - startTime);
+                tvSpeed.Text = testStr;
+
+                ////location.HasAccuracy
+                ////location.Speed
+                ////location.Time
+            }
+        }
+
+        ///<summary>Called when the provider is disabled by the user.</summary>
+        ///<remarks>Implements ILocationListener interface.</remarks>
+        public void OnProviderDisabled(string provider)
+		{
+			Log.Debug(tag, "----- " + provider + " disabled by user");
+		}
+
+	    ///<summary>Called when the provider is enabled by the user.</summary>
+		///<remarks>Implements ILocationListener interface.</remarks>
+		public void OnProviderEnabled(string provider)
+		{
+			Log.Debug(tag, "----- " + provider + " enabled by user");
+		}
+
+	    ///<summary>Called when the provider status changes.</summary>
+		///<remarks>Implements ILocationListener interface.</remarks>
+		public void OnStatusChanged(string provider, Availability status, Bundle extras)
+		{
+			Log.Debug(tag, "----- " + provider + " availability has changed to " + status.ToString());
+		}
+
+        /// <summary>Activity component event handler - btnStartStop click.</summary>
+        public void btnStartStop_OnClick()
+        {
+            if (locMgr.AllProviders.Contains(LocationManager.GpsProvider)
+                && locMgr.IsProviderEnabled(LocationManager.GpsProvider))
+            {
+                // Handle location manager updates by button state (label value)
+                HandleLocationUpdates( btnStartStop.Text == GetString(Resource.String.btn_start) );
+            }
+            else
+            {
+                Toast.MakeText(this, "The GPS provider does not exist or is not enabled!", ToastLength.Long).Show();
+            }
+        }
+
+        /// <summary>Handle updates of location manager.</summary>
+        /// <param name="enable">True requests location updates, False removes location updates.</param>
+        private void HandleLocationUpdates(bool enable)
+        {
+            if (enable)
+            {
                 // pass in the provider (GPS), 
                 // the minimum time between updates (in seconds), 
                 // the minimum distance the user needs to move to generate an update (in meters),
@@ -99,87 +200,27 @@ namespace Location
                 //Log.Debug(tag, "Starting location updates with " + locationProvider.ToString());
                 //locMgr.RequestLocationUpdates(locationProvider, 2000, 1, this);
 
-
-                // Requestes GPS provider for updates every min 2000ms & min 1m
-                if (locMgr.AllProviders.Contains(LocationManager.GpsProvider)
-                    && locMgr.IsProviderEnabled(LocationManager.GpsProvider))
-                {
-                    locMgr.RequestLocationUpdates(LocationManager.GpsProvider, 2000, 1, this);
-                }
-                else
-                {
-                    Toast.MakeText(this, "The GPS provider does not exist or is not enabled!", ToastLength.Long).Show();
-                }
-            };
-		}
-
-		protected override void OnPause()
-		{
-			base.OnPause();
-
-			// stop sending location updates when the application goes into the background
-			// to learn about updating location in the background, refer to the Backgrounding guide
-			// http://docs.xamarin.com/guides/cross-platform/application_fundamentals/backgrounding/
-
-			// RemoveUpdates takes a pending intent - here, we pass the current Activity
-			locMgr.RemoveUpdates(this);
-			Log.Debug(tag, "----- Location updates paused because application is entering the background");
-		}
-
-		protected override void OnStop()
-		{
-			base.OnStop();
-			Log.Debug(tag, "----- OnStop called");
-		}
-
-	    ///<summary>Called when the location has changed.</summary>
-		///<remarks>Implements ILocationListener interface.</remarks>
-		public void OnLocationChanged(Android.Locations.Location location)
-		{
-            tvLatitude.Text = "Latitude: " + location.Latitude.ToString();
-            tvLongitude.Text = "Longitude: " + location.Longitude.ToString();
-            tvProvider.Text = "Provider: " + location.Provider.ToString();
-
-            //Decide what to do on location update
-            if (!distance.HasValue)
-            {
-                //First location update - do initializations
-                Log.Debug(tag, "----- Location initialized");
-                lastLatLng.Assign(location);
-                distance = 0.0;
+                // Requests GPS updates every 1000ms OR 1m
+                locMgr.RequestLocationUpdates(LocationManager.GpsProvider, 1000, 1, this);
+                btnStartStop.Text = GetString(Resource.String.btn_stop);
+                Log.Debug(tag, "----- Requests location updates");
             }
             else
             {
-                //Next location updates - calculate distance step as actual - last & whole distance 
-                Log.Debug(tag, "----- Location changed");
-                actLatLng.Assign(location);
-                distance += Utils.HaversineDistance(lastLatLng, actLatLng, Utils.DistanceUnit.Kilometers);
-                lastLatLng.Assign(actLatLng);
-                tvDistance.Text = String.Format("Distance: {0:0.000} Km", distance);
+                // RemoveUpdates takes a pending intent - here, we pass the current Activity
+                locMgr.RemoveUpdates(this);
+                btnStartStop.Text = GetString(Resource.String.btn_start);
+                tvLatitude.Text = "";
+                tvLongitude.Text = "";
+                tvProvider.Text = "";
+                tvDistance.Text = "";
+                tvSpeed.Text = "";
+                startTime = 0;
+                distance = null;
+                Log.Debug(tag, "----- Removes location updates");
             }
         }
-
-        ///<summary>Called when the provider is disabled by the user.</summary>
-        ///<remarks>Implements ILocationListener interface.</remarks>
-        public void OnProviderDisabled(string provider)
-		{
-			Log.Debug(tag, "----- " + provider + " disabled by user");
-		}
-
-	    ///<summary>Called when the provider is enabled by the user.</summary>
-		///<remarks>Implements ILocationListener interface.</remarks>
-		public void OnProviderEnabled(string provider)
-		{
-			Log.Debug(tag, "----- " + provider + " enabled by user");
-		}
-
-	    ///<summary>Called when the provider status changes.</summary>
-		///<remarks>Implements ILocationListener interface.</remarks>
-		public void OnStatusChanged(string provider, Availability status, Bundle extras)
-		{
-			Log.Debug(tag, "----- " + provider + " availability has changed to " + status.ToString());
-		}
-	}
+    }
 }
 
 
--- Location/Location/Resources/Resource.designer.cs
+++ Location/Location/Resources/Resource.designer.cs
@@ -60,20 +60,23 @@ namespace Location
 		public partial class Id
 		{
 			
+			// aapt resource value: 0x7f050000
+			public const int btnStartStop = 2131034112;
+			
 			// aapt resource value: 0x7f050004
-			public const int distance = 2131034116;
+			public const int tvDistance = 2131034116;
 			
 			// aapt resource value: 0x7f050001
-			public const int latitude = 2131034113;
+			public const int tvLatitude = 2131034113;
 			
 			// aapt resource value: 0x7f050002
-			public const int longitude = 2131034114;
-			
-			// aapt resource value: 0x7f050000
-			public const int myButton = 2131034112;
+			public const int tvLongitude = 2131034114;
 			
 			// aapt resource value: 0x7f050003
-			public const int provider = 2131034115;
+			public const int tvProvider = 2131034115;
+			
+			// aapt resource value: 0x7f050005
+			public const int tvSpeed = 2131034117;
 			
 			static Id()
 			{
@@ -104,11 +107,14 @@ namespace Location
 		public partial class String
 		{
 			
-			// aapt resource value: 0x7f040001
-			public const int app_name = 2130968577;
-			
 			// aapt resource value: 0x7f040000
-			public const int hello = 2130968576;
+			public const int app_name = 2130968576;
+			
+			// aapt resource value: 0x7f040001
+			public const int btn_start = 2130968577;
+			
+			// aapt resource value: 0x7f040002
+			public const int btn_stop = 2130968578;
 			
 			static String()
 			{
