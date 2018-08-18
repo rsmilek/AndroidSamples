d2c106f66ee59f4f0343096975d5744347f2e81b
rsmilek <rsmilek@users.noreply.github.com>
2018-08-18 13:05:47 +0200

    MyToolbarApp.csproj: + Added VS studio generated template only.

--- SingleViewApp/SingleViewApp/MainActivity.cs
+++ SingleViewApp/SingleViewApp/MainActivity.cs
@@ -12,31 +12,54 @@ namespace SingleViewApp
     [Activity(Label = "@string/app_name", MainLauncher = true, LaunchMode = Android.Content.PM.LaunchMode.SingleTop, Icon = "@drawable/icon")]
     public class MainActivity : AppCompatActivity
     {
-
         int count = 1;
 
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
             SetContentView(Resource.Layout.main);
+
             var toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
             if (toolbar != null)
             {
                 SetSupportActionBar(toolbar);
-                SupportActionBar.SetDisplayHomeAsUpEnabled(false);
-                SupportActionBar.SetHomeButtonEnabled(false);
+                //SupportActionBar.SetDisplayHomeAsUpEnabled(true);
+                //SupportActionBar.SetHomeButtonEnabled(true);
             }
 
-            // Get our button from the layout resource,
-            // and attach an event to it
+            
+
+            // Get our button from the layout resource and attach an event to it
             var clickButton = FindViewById<Button>(Resource.Id.my_button);
 
             clickButton.Click += (sender, args) =>
               {
                   clickButton.Text = string.Format("{0} clicks!", count++);
               };
-
         }
+
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            //change main_compat_menu
+            MenuInflater.Inflate(Resource.Menu.menu, menu);
+            return base.OnCreateOptionsMenu(menu);
+        }
+
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            switch (item.ItemId)
+            {
+                case Resource.Id.action_edit:
+                    Toast.MakeText(this, "You pressed edit action!", ToastLength.Short).Show();
+                    break;
+                case Resource.Id.action_save:
+                    Toast.MakeText(this, "You pressed save action!", ToastLength.Short).Show();
+                    break;
+            }
+            return base.OnOptionsItemSelected(item);
+        }
+
     }
 }
 
